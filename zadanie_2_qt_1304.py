f5e54b6410e5244f4881126cba18219c105c91d6
davep777 <164636436+davep777@users.noreply.github.com>
2025-06-23 13:13:32 +0200

    Update file

--- zadanie_2_qt_1304.py
+++ zadanie_2_qt_1304.py
@@ -1,164 +1,79 @@
-from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex, QSortFilterProxyModel
-from PySide6.QtGui import QIcon, QPixmap, QBrush
-from PySide6.QtWidgets import QApplication, QMainWindow, QTableView, QVBoxLayout, QWidget, QComboBox, QMessageBox, QHeaderView
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QComboBox, QMessageBox
+from PyQt6.QtGui import QBrush, QColor, QIcon, QPixmap
 import sys
-from dataclasses import dataclass
 from datetime import date, timedelta
 
-# Reprezentacja książki
-@dataclass
-class Book:
-    title: str
-    author: str
-    genre: str
-    status: str  # "dostępna" lub "wypożyczona"
-    borrower_first: str = ""
-    borrower_last: str = ""
-    borrow_date: date = None
-
-# Model tabelaryczny prezentujący dane o książkach
-class LibraryModel(QAbstractTableModel):
-    def __init__(self, books=None):
-        super().__init__()
-        self._books = books or []
-        self.headers = ["Tytuł", "Autor", "Gatunek", "Status", "Czytelnik", "Data wypożyczenia"]
-
-    def rowCount(self, parent=QModelIndex()):
-        return len(self._books)
-
-    def columnCount(self, parent=QModelIndex()):
-        return len(self.headers)
-
-    def data(self, index, role=Qt.DisplayRole):
-        if not index.isValid():
-            return None
-        book = self._books[index.row()]
-        column = index.column()
-
-        # Wyświetlanie danych tekstowych
-        if role == Qt.DisplayRole:
-            if column == 0:
-                return book.title
-            elif column == 1:
-                return book.author
-            elif column == 2:
-                return book.genre
-            elif column == 3:
-                return book.status
-            elif column == 4:
-                # Jeśli książka jest wypożyczona, pokaż imię i nazwisko osoby, która ją wypożyczyła
-                return f"{book.borrower_first} {book.borrower_last}" if book.status == "wypożyczona" else ""
-            elif column == 5:
-                if book.status == "wypożyczona" and book.borrow_date:
-                    return book.borrow_date.strftime("%Y-%m-%d")
-                else:
-                    return ""
-        
-        # Dodanie ikony w kolumnie Status
-        if role == Qt.DecorationRole:
-            if column == 3:
-                pixmap = QPixmap(16, 16)
-                if book.status == "dostępna":
-                    pixmap.fill(Qt.green)
-                else:
-                    pixmap.fill(Qt.red)
-                return QIcon(pixmap)
-        
-        # Jeśli książka jest wypożyczona, a czas wypożyczenia przekracza 14 dni, oznacz tło na czerwono
-        if role == Qt.BackgroundRole:
-            if book.status == "wypożyczona" and book.borrow_date:
-                if (date.today() - book.borrow_date).days > 14:
-                    return QBrush(Qt.red)
-        return None
-
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            if section < len(self.headers):
-                return self.headers[section]
-        return super().headerData(section, orientation, role)
-
-    def sort(self, column, order):
-        # Sortowanie według wybranej kolumny
-        if column == 0:
-            self._books.sort(key=lambda book: book.title)
-        elif column == 1:
-            self._books.sort(key=lambda book: book.author)
-        elif column == 5:
-            # Jeśli data wypożyczenia nie istnieje, traktujemy ją jak minimalną datę
-            self._books.sort(key=lambda book: book.borrow_date if book.borrow_date else date.min)
-        # Inne kolumny można dodać w razie potrzeby
-        if order == Qt.DescendingOrder:
-            self._books.reverse()
-        self.layoutChanged.emit()
-
-# Model proxy do filtrowania książek według statusu
-class LibraryFilterProxyModel(QSortFilterProxyModel):
-    def __init__(self):
-        super().__init__()
-        self.statusFilter = None  # "wypożyczona", "dostępna" lub None dla wszystkich
-
-    def setStatusFilter(self, status):
-        self.statusFilter = status
-        self.invalidateFilter()
-
-    def filterAcceptsRow(self, source_row, source_parent):
-        if self.statusFilter is None or self.statusFilter == "Wszystkie":
-            return True
-        index = self.sourceModel().index(source_row, 3, source_parent)  # kolumna status ma index 3
-        data = self.sourceModel().data(index, Qt.DisplayRole)
-        return data == self.statusFilter
-
-# Główne okno aplikacji
-class MainWindow(QMainWindow):
+class OknoBiblioteki(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Biblioteka")
         self.resize(800, 600)
 
-        # Dane przykładowe –  książki
-        self.books = [
-            Book("Drużyna Pierścieni", "J.R.R.Tolkien", "Fantasy", "dostępna"),
-            Book("Lalka", "Bolesław Prus", "Powieść", "wypożyczona", "Jan", "Kowalski", date.today() - timedelta(days=10)),
-            Book("Quo Vadis", "Henryk Sienkiewicz", "Historyczna", "wypożyczona", "Anna", "Nowak", date.today() - timedelta(days=20)),
-            Book("Krew Elfów", "Andrzej Sapkowski", "Fantasy", "dostępna"),
+        # Lista książek
+        self.ksiazki = [
+            ["Drużyna Pierścieni", "J.R.R. Tolkien", "Fantasy", "dostępna", "", "", ""],
+            ["Lalka", "Bolesław Prus", "Powieść", "wypożyczona", "Jan", "Kowalski", (date.today() - timedelta(days=10)).strftime("%Y-%m-%d")],
+            ["Quo Vadis", "Henryk Sienkiewicz", "Historyczna", "wypożyczona", "Anna", "Nowak", (date.today() - timedelta(days=20)).strftime("%Y-%m-%d")],
+            ["Krew Elfów", "Andrzej Sapkowski", "Fantasy", "dostępna", "", "", ""]
         ]
 
-        self.model = LibraryModel(self.books)
-        self.proxyModel = LibraryFilterProxyModel()
-        self.proxyModel.setSourceModel(self.model)
+        # Tworzenie tabeli
+        self.tabela = QTableWidget()
+        self.tabela.setColumnCount(7)
+        self.tabela.setHorizontalHeaderLabels(["Tytuł", "Autor", "Gatunek", "Status", "Czytelnik", "Nazwisko", "Data wypożyczenia"])
+        self.tabela.setSortingEnabled(True)
 
-        # Widok tabelaryczny
-        self.tableView = QTableView()
-        self.tableView.setModel(self.proxyModel)
-        self.tableView.setSortingEnabled(True)
-        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        # Wypełnianie tabeli danymi
+        self.wypelnij_tabele()
 
-        # ComboBox do filtrowania po statusie
-        self.filterCombo = QComboBox()
-        self.filterCombo.addItems(["Wszystkie", "wypożyczona", "dostępna"])
-        self.filterCombo.currentTextChanged.connect(self.proxyModel.setStatusFilter)
+        # Filtr statusu książek
+        self.lista_filtr = QComboBox()
+        self.lista_filtr.addItems(["Wszystkie", "wypożyczona", "dostępna"])
+        self.lista_filtr.currentTextChanged.connect(self.filtruj_ksiazki)
 
-        # Układ – filtr na górze, a pod nim tabela
-        centralWidget = QWidget()
-        self.setCentralWidget(centralWidget)
-        layout = QVBoxLayout(centralWidget)
-        layout.addWidget(self.filterCombo)
-        layout.addWidget(self.tableView)
+        # Układ interfejsu
+        centralny_widget = QWidget()
+        self.setCentralWidget(centralny_widget)
+        uklad = QVBoxLayout(centralny_widget)
+        uklad.addWidget(self.lista_filtr)
+        uklad.addWidget(self.tabela)
 
-        # Po uruchomieniu aplikacji sprawdzamy, czy są wypożyczone książki po terminie
-        self.checkOverdueBooks()
+        # Sprawdzanie przeterminowanych książek
+        self.sprawdz_przeterminowane()
 
-    def checkOverdueBooks(self):
-        overdue_books = [book for book in self.books if book.status == "wypożyczona" and book.borrow_date and (date.today() - book.borrow_date).days > 14]
-        if overdue_books:
-            msg = "Następujące książki są przedawnione:\n"
-            for book in overdue_books:
-                days_overdue = (date.today() - book.borrow_date).days - 14
-                msg += f"{book.title} – {days_overdue} dni po terminie\n"
+    def wypelnij_tabele(self):
+        self.tabela.setRowCount(len(self.ksiazki))
+        for i, ksiazka in enumerate(self.ksiazki):
+            for j, wartosc in enumerate(ksiazka):
+                item = QTableWidgetItem(wartosc)
+
+                # Dodanie ikonki dla statusu 
+                if j == 3:  # Kolumna statusu
+                    ikona = QPixmap(16, 16)
+                    ikona.fill(QColor("green") if ksiazka[3] == "dostępna" else QColor("red"))
+                    item.setIcon(QIcon(ikona))
+
+                # Podświetlenie przeterminowanych książek
+                if j == 6 and ksiazka[3] == "wypożyczona" and ksiazka[6]:
+                    data_wypozyczenia = date.fromisoformat(ksiazka[6])
+                    if (date.today() - data_wypozyczenia).days > 14:
+                        item.setBackground(QBrush(QColor("red")))
+
+                self.tabela.setItem(i, j, item)
+
+    def filtruj_ksiazki(self, status):
+        for i in range(self.tabela.rowCount()):
+            pokaz = (status == "Wszystkie" or self.tabela.item(i, 3).text() == status)
+            self.tabela.setRowHidden(i, not pokaz)
+
+    def sprawdz_przeterminowane(self):
+        przeterminowane = [ksiazka for ksiazka in self.ksiazki if ksiazka[3] == "wypożyczona" and ksiazka[6] and (date.today() - date.fromisoformat(ksiazka[6])).days > 14]
+        if przeterminowane:
+            msg = "Następujące książki są przedawnione:\n" + "\n".join([f"{ksiazka[0]} – {(date.today() - date.fromisoformat(ksiazka[6])).days - 14} dni po terminie" for ksiazka in przeterminowane])
             QMessageBox.warning(self, "Książki przedawnione", msg)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    okno = OknoBiblioteki()
+    okno.show()
     sys.exit(app.exec())
